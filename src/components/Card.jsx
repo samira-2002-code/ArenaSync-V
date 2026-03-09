9bd12187bbf4ce08ff6ee0150f5144d42b02c964
samira-2002-code <samiraassemlali2@gmail.com>
2026-03-09 12:07:11 +0000

    last commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import StatusBadge from "./StatusBadge";
 
 export default function Card({ tournament }) {
   return (
-    <div className="border bg-gray-200 ml-4 p-8 mr-4 rounded-lg">
+    <div className="border bg-gray-200 ml-4 p-8 mr-4 rounded-lg mb-8">
       <h2>{tournament.title}</h2>
 
       <p>{tournament.description}</p>
